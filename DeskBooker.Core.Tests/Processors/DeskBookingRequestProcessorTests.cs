7f045d40f21f9cbfdf434617fe7f3217e47a8cbd
raja <rajass89@gmail.com>
2024-01-25 12:15:30 +0530

    test cases for saving BookDesk

--- DeskBooker.Core.Tests/Processors/DeskBookingRequestProcessorTests.cs
+++ DeskBooker.Core.Tests/Processors/DeskBookingRequestProcessorTests.cs
@@ -1,23 +1,29 @@
 using System;
+using System.Threading;
+using DeskBooker.Core.DataInterface;
 using DeskBooker.Core.Models;
 using DeskBooker.Core.Processors;
+using Moq;
 using Xunit;
 
 namespace DeskBooker.Core.Tests
 {
     public class DeskBookingRequestProcessorTests
     {
-        public DeskBookingRequestProcessor _deskBookingRequest { get; }
+        public DeskBookingRequestProcessor _deskBookingService { get; }
+        private readonly Mock<IDeskBookingRepository> _deskBookingRepository;
+
         public DeskBookingRequestProcessorTests()
         {
-            _deskBookingRequest = new DeskBookingRequestProcessor();
+            _deskBookingRepository = new Mock<IDeskBookingRepository>();
+            _deskBookingService = new DeskBookingRequestProcessor(_deskBookingRepository.Object);
         }
 
         [Fact]
         public void BookingDesk_ValidBookingDetails_ReturnBookedDetails()
         {
             // Arrange
-            var bookingDetails = new DeskBooking
+            var bookingDetails = new DeskBookingDTO
             {
                 FirstName = "Raja",
                 LastName = "K",
@@ -26,14 +32,14 @@ namespace DeskBooker.Core.Tests
             };
 
             // Act
-            var result = _deskBookingRequest.BookDesk(bookingDetails);
+            var result = _deskBookingService.BookDesk(bookingDetails);
 
             // Assert
             Assert.NotNull(result);
             Assert.Equal(bookingDetails.FirstName, result.FirstName);
             Assert.Equal(bookingDetails.LastName, result.LastName);
             Assert.Equal(bookingDetails.EmailId, result.EmailId);
-            Assert.Equal(bookingDetails.BookingDate, result.BookingDate);
+            Assert.Equal(bookingDetails.BookingDate.Date, result.BookingDate.Date);
 
         }
 
@@ -41,9 +47,35 @@ namespace DeskBooker.Core.Tests
         public void BookDesk_ParameterAsNull_ReturnsException()
         {
             //Assert
-          var exceptionResult =  Assert.Throws<ArgumentNullException>(() => _deskBookingRequest.BookDesk(null));
+          var exceptionResult =  Assert.Throws<ArgumentNullException>(() => _deskBookingService.BookDesk(null));
           
          Assert.Equal("Input parameter cannot be null",exceptionResult.ParamName);
+        }
+        
+        [Fact]
+        public void BookDesk_PassBookDetails_ReturnsSavedData()
+        {
+            DeskBookingEntity deskBookingEntity = null;
+            var BookDetails = new DeskBookingDTO {
+                FirstName = "Raja",
+                LastName = "K",
+                EmailId = "raja.k@yahoo.com",
+                BookingDate = DateTime.Now
+            };
+
+            _deskBookingRepository.Setup(x => x.SaveBookDesk(It.IsAny<DeskBookingEntity>()))
+            .Callback((DeskBookingEntity callBackResult)=> deskBookingEntity = callBackResult );
+
+            _deskBookingService.BookDesk(BookDetails);
+            
+            _deskBookingRepository.Verify(x => x.SaveBookDesk(It.IsAny<DeskBookingEntity>()),Times.Once);
+
+            Assert.NotNull(deskBookingEntity);
+            Assert.Equal(BookDetails.FirstName, deskBookingEntity!.FirstName);
+            Assert.Equal(BookDetails.LastName, deskBookingEntity.LastName);
+            Assert.Equal(BookDetails.EmailId, deskBookingEntity.EmailId);
+            Assert.Equal(BookDetails.BookingDate.Date, deskBookingEntity.BookingDate.Date);
+            
         }
     }
 }
--- DeskBooker.Core/Processors/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processors/DeskBookingRequestProcessor.cs
@@ -2,20 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DeskBooker.Core.DataInterface;
 using DeskBooker.Core.Models;
 
 namespace DeskBooker.Core.Processors
 {
     public class DeskBookingRequestProcessor
     {
-        
-        public DeskBooking BookDesk(DeskBooking deskBooking)
+        private readonly IDeskBookingRepository _deskBookingRepository;
+        public DeskBookingRequestProcessor(IDeskBookingRepository deskBookingRepository)
         {
-            if(deskBooking is null)
+            _deskBookingRepository = deskBookingRepository;
+        }
+
+        public DeskBookingDTO BookDesk(DeskBookingDTO deskBooking)
+        {
+            if (deskBooking is null)
             {
                 throw new ArgumentNullException("Input parameter cannot be null");
             }
+
+          _deskBookingRepository.SaveBookDesk(MapObject<DeskBookingEntity>(deskBooking));
+
             return deskBooking;
+        }
+
+        private static T MapObject<T>(DeskBookingDTO data) where T : DeskBooking, new()
+        {
+            return new T {
+                FirstName = "Raja",
+                LastName = "K",
+                EmailId = "raja.k@yahoo.com",
+                BookingDate = DateTime.Now
+            };
         }
     }
 }
